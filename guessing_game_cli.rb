332e087718d75621814c4802a04e891bf0291e9a
Robert Keller <robert_keller44@icloud.com>
2020-01-31 18:16:42 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,25 @@
+require 'pry'
 # Code your solution here!
+def run_guessing_game
+# Generate and store a rand num btwn 1 & 6
+number = (rand(6) + 1)
+
+# Prompt to guess the number
+input = gets
+
+# compare the input and the number
+
+if number == input
+  puts "You guessed the correct number!"
+  
+elsif input == "exit"
+  puts "Goodbye!"
+  
+else
+    puts "Sorry! The computer guessed #{number}."
+end
+# print out one of these statements
+# "You guessed the correct number!"
+# Sorry! The computer guessed <number>
+# if input == exit => "Goodbye!"
+end
--- spec/guessing_cli_spec.rb
+++ spec/guessing_cli_spec.rb
@@ -9,15 +9,15 @@ describe "Guessing CLI" do
     end
 
     it "responds to an incorrect guess" do
-      allow(self).to receive(:rand).and_return(5)
+      allow(self).to receive(:rand).and_return(1)
 
       expect(self).to receive(:gets).and_return("2")
 
-      expect { run_guessing_game }.to output(/Sorry! The computer guessed 6./).to_stdout
+      expect { run_guessing_game }.to output(/Sorry! The computer guessed 2./).to_stdout
     end
 
     it "responds to a correct guess" do
-      allow(self).to receive(:rand).and_return(1)
+      allow(self).to receive(:rand).and_return(2)
 
       expect(self).to receive(:gets).and_return("2")
 
