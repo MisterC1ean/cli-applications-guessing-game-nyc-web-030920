c07bd56e80f175753179282243194a8ced8e0b75
Robert Keller <robert_keller44@icloud.com>
2020-01-31 18:28:04 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,11 +2,10 @@ require 'pry'
 def run_guessing_game
   # Generate and store a rand num btwn 1 & 6
 number = rand(6) + 1
-number = number.to_s
 input = gets.chomp
 
   # compare the input and the number puts something based on comparison
-if number == input
+if number.to_s == input
   puts "You guessed the correct number!"
   
 elsif input == "exit"
